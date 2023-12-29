61f76e7c67a4cf97f9b712652779d8ca7c8fac70
yayat <muhyayat199@gmail.com>
2023-12-29 20:58:23 +0800

    membuat footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <span>© Copyright 2021, Powered by <a href="">Freelancer Shakil</a></span>
+                <span>© Copyright 2021, Powered by <a href="">Aqilah / Askar</a></span>
             </div>
         </div>
     </div>
